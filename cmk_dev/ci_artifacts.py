b98f7071070c98a8b309588d01681830745e14dd
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-09-17 14:36:51 +0200

    Always return a valid JSON output on error
    ... exception messages are printed with the logger on error level and
    an empty JSON is returned as well to ensure the output of ci-artifacts
    is always machine processable.
    
    CMK-26067
    
    Change-Id: Ia6857e88d9066484703f21beb0f3188b13b2c741

--- cmk_dev/ci_artifacts.py
+++ cmk_dev/ci_artifacts.py
@@ -1179,7 +1179,8 @@ def main() -> None:
                 asyncio.run(stop_build(args=args, ongoing_build_info=ongoing_build_info))
                 log().debug("Stopping ongoing job")
     except Fatal as exc:
-        print(exc, file=sys.stderr)
+        log().error("Fatal exception: %s", exc)
+        print(json.dumps({"err": f"Fatal exception: {exc}" }))  # always return a valid JSON to the caller
         raise SystemExit(-1) from exc
 
 
