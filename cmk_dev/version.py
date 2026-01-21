14c95881f99643b4bafaf5f082f75adefb49f4ef
Checkmk release system <noreply@checkmk.com>
2026-01-21 07:27:33 +0000

    Add changelog for 2.0.2

--- cmk_dev/version.py
+++ cmk_dev/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-__version_info__ = ("0", "2", "0")
-__version__ = ".".join(__version_info__)
+__version_info__ = ("2", "0", "2")
+__version__ = '.'.join(__version_info__)
